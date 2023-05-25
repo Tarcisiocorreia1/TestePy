bd427f904e21aed8a4018011ba6b2cd00882b145
Tarcisiocorreia1 <tarcisio00c@gmail.com>
2023-05-25 17:37:13 -0500

    adicionando os arquivos

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello world")
+
+def funcao(x):
+    cont = 0
+    while(x<=1000):
+        cont = x
+        print(x)
+        cont = x + 1
+        x = cont
+
+valor = float(input("digite o valor: "))
+
+funcao(valor) 
